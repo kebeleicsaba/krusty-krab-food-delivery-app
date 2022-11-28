c1a8bc89dee8ac68ad46a3a862d40c9b63637100
Kebelei Csaba <71121076+kebeleicsaba@users.noreply.github.com>
2022-11-28 21:02:26 +0100

    Init expo project with react native elements

---
+++ App.js
@@ -0,0 +1,9 @@
+import { Button, ThemeProvider } from 'react-native-elements';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <Button title="Hey!" />
+    </ThemeProvider>
+  );
+}
