4f9138dabd32279303768ae0bf44204c4a970ef8
Kebelei Csaba <71121076+kebeleicsaba@users.noreply.github.com>
2022-11-28 21:23:54 +0100

    try view

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
+import { View, Alert } from 'react-native';
 import { Button, ThemeProvider } from 'react-native-elements';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <Button title="Hey!" />
+      <View
+       style={{
+        flex: 1,
+        backgroundColor: '#f2f2f2',
+        justifyContent: 'center'
+       }}
+      >
+       <Button title="Hey!" onPress={() => Alert.alert('Siker!')} />
+      </View>
     </ThemeProvider>
   );
 }
