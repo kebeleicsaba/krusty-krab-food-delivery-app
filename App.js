b49302a29753ca5b963701de0c44163fd210b533
Kebelei Csaba <71121076+kebeleicsaba@users.noreply.github.com>
2022-12-28 17:31:24 +0100

    add icons for bottom tabs

--- App.js
+++ App.js
@@ -3,16 +3,45 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MenuScreen from "./screens/Menu";
 import CartScreen from "./screens/Cart";
 import ProfileScreen from "./screens/Profile";
+import { MaterialIcons, Foundation, FontAwesome } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Menu" component={MenuScreen} />
-        <Tab.Screen name="Cart" component={CartScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "#7C1015",
+        }}
+      >
+        <Tab.Screen
+          name="Menu"
+          component={MenuScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name="restaurant-menu" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Cart"
+          component={CartScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Foundation name="shopping-cart" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="user" color={color} size={size} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
