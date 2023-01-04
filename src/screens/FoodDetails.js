e7c2c355e3b701e24a377dd140337679dda6b415
Kebelei Csaba <71121076+kebeleicsaba@users.noreply.github.com>
2023-01-04 18:56:13 +0100

    update food details

--- src/screens/FoodDetails.js
+++ src/screens/FoodDetails.js
@@ -1,16 +1,67 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { useEffect, useState } from "react";
+import styles from "../styles";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db, storage } from "../../config/firebase";
+import { getDownloadURL, ref } from "firebase/storage";
 
 export default function FoodDetailsSrceen({ navigation, route }) {
-  const [menuItem, setMenuItem] = useState();
+  const [FoodName, setFoodName] = useState(route.params.name);
+  const [FoodPrice, setFoodPrice] = useState();
+  const [FoodImage, setFoodImage] = useState("");
+  const [FoodDescription, setFoodDescription] = useState("");
 
   useEffect(() => {
     navigation.setOptions({ title: route.params.name });
-  }, [navigation, menuItem]);
+  }, [navigation, FoodName]);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "foods"),
+      where("name", "==", route.params.name)
+    );
+    getData = async () => {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        const food = doc.data();
+        setFoodPrice(food.price);
+        setFoodDescription(food.description);
+        getDownloadURL(ref(storage, food.image)).then((url) => {
+          const xhr = new XMLHttpRequest();
+          xhr.responseType = "blob";
+          xhr.onload = (event) => {
+            const blob = xhr.response;
+          };
+          xhr.open("GET", url);
+          xhr.send();
+          setFoodImage({ uri: url });
+        });
+      });
+    };
+    getData();
+  }, [db]);
 
   return (
-    <View>
-      <Text>lorem ipsum</Text>
+    <View style={{ ...styles.container, alignItems: "center" }}>
+      <Image
+        style={{
+          width: 150,
+          height: 150,
+          resizeMode: "center",
+          marginTop: 20,
+        }}
+        source={FoodImage}
+      />
+      <Text
+        style={{
+          fontSize: 16,
+          margin: 20,
+          textAlign: "justify",
+        }}
+      >
+        {FoodDescription}
+      </Text>
+      <Text style={{ color: "gray" }}>Price: {FoodPrice} $</Text>
     </View>
   );
 }
