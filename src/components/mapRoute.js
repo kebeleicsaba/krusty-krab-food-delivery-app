1ae575b10700587b239915bda1dfb00c66a66e26
Kebelei Csaba <71121076+kebeleicsaba@users.noreply.github.com>
2023-01-09 02:00:59 +0100

    add gecoding

--- src/components/mapRoute.js
+++ src/components/mapRoute.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Dimensions, Image, Text, View } from "react-native";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
@@ -13,66 +13,82 @@ const krustyKrabCoords = {
 
 export default function MapRoute({ addressCoords }) {
   const [duration, setDuration] = useState();
+  const [adressLoc, setAddressLoc] = useState();
+  const [error, setError] = useState(false);
+
   const getDuration = (result) => {
     if (result) {
       setDuration(result.duration);
     }
   };
 
+  useEffect(() => {
+    setAddressLoc(addressCoords);
+    setError(false);
+  }, [addressCoords]);
+
   return (
     <>
-      <Text>{JSON.stringify(addressCoords)}</Text>
-      <MapView
-        initialRegion={{
-          latitude: addressCoords.latitude,
-          longitude: addressCoords.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        style={{
-          width: Dimensions.get("window").width - 40,
-          height: Dimensions.get("window").width - 40,
-          alignSelf: "center",
-        }}
-      >
-        <MapViewDirections
-          origin={krustyKrabCoords}
-          destination={addressCoords}
-          apikey={config.googleMapsApiKey} // insert your API Key here
-          strokeWidth={4}
-          strokeColor="#FE0002"
-          onReady={getDuration}
-        />
-
-        <Marker
-          coordinate={{
-            latitude: krustyKrabCoords.latitude,
-            longitude: krustyKrabCoords.longitude,
-          }}
-        >
-          <Image
-            source={require("../images/krusty_krab_marker.png")}
-            style={{ height: 40, resizeMode: "contain" }}
-          />
-        </Marker>
-        <Marker
-          coordinate={{
-            latitude: addressCoords.latitude,
-            longitude: addressCoords.longitude,
-          }}
-        >
-          <View style={{ alignItems: "center" }}>
-            <Image
-              source={require("../images/home_marker.png")}
-              style={{ height: 40, resizeMode: "contain", flex: 1 }}
+      {!error ? (
+        <>
+          <MapView
+            initialRegion={{
+              latitude: addressCoords.latitude,
+              longitude: addressCoords.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            style={{
+              width: Dimensions.get("window").width - 40,
+              height: Dimensions.get("window").width - 40,
+              alignSelf: "center",
+            }}
+          >
+            <MapViewDirections
+              origin={krustyKrabCoords}
+              destination={adressLoc}
+              apikey={config.googleMapsApiKey}
+              strokeWidth={4}
+              strokeColor="#FE0002"
+              onReady={getDuration}
+              onError={() => setError(true)}
             />
-            <Text style={{ color: "white", fontSize: 10 }}>Your Address</Text>
-          </View>
-        </Marker>
-      </MapView>
-      <Text style={{ textAlign: "center" }}>
-        Duration: {Math.ceil(duration)} minutes.
-      </Text>
+
+            <Marker
+              coordinate={{
+                latitude: krustyKrabCoords.latitude,
+                longitude: krustyKrabCoords.longitude,
+              }}
+            >
+              <Image
+                source={require("../images/krusty_krab_marker.png")}
+                style={{ height: 40, resizeMode: "contain" }}
+              />
+            </Marker>
+            <Marker
+              coordinate={{
+                latitude: addressCoords.latitude,
+                longitude: addressCoords.longitude,
+              }}
+            >
+              <View style={{ alignItems: "center" }}>
+                <Image
+                  source={require("../images/home_marker.png")}
+                  style={{ height: 40, resizeMode: "contain", flex: 1 }}
+                />
+                <Text style={{ color: "white", fontSize: 10 }}>
+                  Your Address
+                </Text>
+              </View>
+            </Marker>
+          </MapView>
+          <Text style={{ textAlign: "center" }}>
+            Duration: {Math.ceil(duration)} minutes.
+          </Text>
+        </>
+      ) : (
+        <Text style={{ color: "red" }}> We cannot deliver here!</Text>
+      )}
     </>
   );
 }
--- src/screens/Cart/Order.js
+++ src/screens/Cart/Order.js
@@ -7,25 +7,53 @@ import useUser from "../../hooks/useUser";
 import useCart from "../../hooks/useCart";
 import useLocation from "../../hooks/useLocation";
 import MapRoute from "../../components/mapRoute";
+import Constants from "expo-constants";
+import Geocoder from "react-native-geocoding";
+
+const { config } = Constants.manifest.extra;
+Geocoder.init(config.googleMapsApiKey, { language: "en" });
 
 export default function OrderModal({ navigation }) {
   const [fullName, setFullName] = useState("");
   const [addressLatitude, setAddressLatitude] = useState(false);
   const [addressLongitude, setAddressLongitude] = useState(false);
+  const [address, setAddress] = useState("");
+  const [addressChanged, setAddressChanged] = useState(false);
+  const [geoError, setGeoError] = useState(false);
   const { location, status } = useLocation();
   const { cart } = useCart();
   const { user } = useUser();
+
+  const newAddress = () => {
+    if (address) {
+      Geocoder.from(address)
+        .then((json) => {
+          const newCoords = json.results[0].geometry.location;
+          setAddressLatitude(newCoords.lat);
+          setAddressLongitude(newCoords.lng);
+          setGeoError(false);
+          setAddressChanged(!addressChanged);
+        })
+        .catch((error) => setGeoError(true));
+    }
+  };
 
   useEffect(() => {
     if (status === "granted" && location) {
       setAddressLatitude(location.coords.latitude);
       setAddressLongitude(location.coords.longitude);
+      Geocoder.from(location.coords)
+        .then((json) => {
+          setAddress(json.results[0].formatted_address);
+          setGeoError(false);
+        })
+        .catch((error) => setGeoError(true));
     }
-  }, [addressLongitude, location]);
+  }, [location]);
 
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: "row", paddingVertical: 40 }}>
+      <View style={{ flexDirection: "row", paddingTop: 40, marginBottom: 30 }}>
         <View style={{ minWidth: 80, alignItems: "center" }}>
           <AntDesign
             name="close"
@@ -46,7 +74,7 @@ export default function OrderModal({ navigation }) {
           Order
         </Text>
       </View>
-      {!addressLongitude || !location ? (
+      {!addressLongitude && status === "granted" ? (
         <LoadingEffect />
       ) : (
         <>
@@ -57,8 +85,10 @@ export default function OrderModal({ navigation }) {
               paddingBottom: 20,
             }}
           >
+            {/*
             <Text>{JSON.stringify(cart)}</Text>
             <Text>uid: {user.uid}</Text>
+          */}
 
             <Text> Full name:</Text>
             <TextInput
@@ -67,20 +97,60 @@ export default function OrderModal({ navigation }) {
               onChangeText={setFullName}
               placeholder="Full name"
             />
-            <Text> Address:</Text>
-            <MapRoute
-              addressCoords={{
-                latitude: addressLatitude,
-                longitude: addressLongitude,
+
+            <Text>
+              {" "}
+              Address:{" "}
+              {geoError && <Text style={{ color: "red" }}>Wrong address!</Text>}
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                paddingBottom: 8,
+                paddingTop: 3,
               }}
-            />
+            >
+              <TextInput
+                style={{
+                  ...styles.input,
+                  marginBottom: 0,
+                  marginTop: 0,
+                  flex: 1,
+                }}
+                placeholder="Your address"
+                value={address}
+                onChangeText={(value) => {
+                  setAddress(value);
+                  setAddressChanged(true);
+                }}
+              />
+              {addressChanged && (
+                <AntDesign
+                  name="check"
+                  size={24}
+                  color="black"
+                  style={{ paddingHorizontal: 10 }}
+                  onPress={newAddress}
+                />
+              )}
+            </View>
+            {addressLongitude && !addressChanged && !geoError && (
+              <MapRoute
+                addressCoords={{
+                  latitude: addressLatitude,
+                  longitude: addressLongitude,
+                }}
+              />
+            )}
           </ScrollView>
           <Pressable
             style={{
               ...styles.button,
               paddingVertical: 10,
               paddingHorizontal: 20,
-              marginBottom: 40,
+              marginTop: 20,
+              marginBottom: 25,
               marginHorizontal: 30,
             }}
             onPress={() => null}
