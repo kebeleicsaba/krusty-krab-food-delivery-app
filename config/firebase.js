c77729a8ba5f25c0e6cbea87183a519905335a54
Kebelei Csaba <71121076+kebeleicsaba@users.noreply.github.com>
2023-01-23 18:04:29 +0100

    fix firebase AsyncStorage warning

--- config/firebase.js
+++ config/firebase.js
@@ -3,12 +3,21 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import Constants from "expo-constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth/react-native";
 
 const { config } = Constants.manifest.extra;
 
 const app = initializeApp(config.firebase);
 
+initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
 export const storage = getStorage(app);
 export const db = getFirestore();
-export const auth = getAuth();
+export const auth = getAuth(app);
 export default app;
